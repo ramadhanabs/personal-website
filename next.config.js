2f1bb8856f40b06334f631e02f75ae621b00812a
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2021-10-28 08:04:23 +0700

    update configuration

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
-}
+  webpack(config) {
+    config.module.rule.push({
+      test: /\.md$/,
+      use: 'raw-loader',
+    });
+    return config;
+  },
+};
